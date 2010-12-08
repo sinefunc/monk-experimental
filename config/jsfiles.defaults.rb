975f0676305b5b9330e279ec8a75a2768de36bee
Rico Sta. Cruz <rico@ambiescent.com>
2010-12-09 04:16:17 +0800

    Fix the jsfiles bug where the wrong JS URLs are being used.

--- config/jsfiles.defaults.rb
+++ config/jsfiles.defaults.rb
@@ -1,4 +1,4 @@
 class Main
   files = Dir[root(%w[public js {jquery,lib,app}.*.js])]
-  set :js_files, JsFiles.new(files, :prefix => '/js')
+  set :js_files, JsFiles.new(files, :prefix => '/js/')
 end
